95482d914b49e8a6643d866942790fb310c5ecb4
kujawapatryk <homik363@wp.pl>
2023-12-16 17:19:03 +0100

    Adding card fetching with pagination.

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PaginatedRequest;
 use App\Repositories\CardRepository;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CardController extends Controller
@@ -13,8 +15,18 @@ class CardController extends Controller
     {
         $this->cardRepository = $cardRepository;
     }
-    public function index(){
+    public function index(PaginatedRequest $request): JsonResponse
+    {
+        try {
+        $validatedData = $request->validated();
+        $perPage = $validatedData['limit'] ?? config('app.pagination.per_page');
+        $cards = $this->cardRepository->getAllPaginated($perPage);
 
+        return response()->json($cards);
+
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'An error occurred while fetching data.'], 500);
+        }
     }
 
     public function show(Request $request){
--- app/Repositories/CardRepository.php
+++ app/Repositories/CardRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repositories;
 
 use App\Models\Card;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class CardRepository
 {
@@ -11,6 +13,10 @@ class CardRepository
     public function __construct(Card $card)
     {
         $this->card = $card;
+    }
+
+    public function getAllPaginated(int $perPage): LengthAwarePaginator {
+        return $this->card->paginate($perPage);
     }
 
 }
