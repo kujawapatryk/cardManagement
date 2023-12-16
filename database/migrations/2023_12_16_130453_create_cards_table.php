54470d0b0b22322d254cb3570731e493d8e3d042
kujawapatryk <homik363@wp.pl>
2023-12-16 14:05:37 +0100

    Add migration for 'cards' table

---
+++ database/migrations/2023_12_16_130453_create_cards_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->string('card_number', 20);
+            $table->string('pin', 4);
+            $table->dateTime('activation_date');
+            $table->date('expiry_date');
+            $table->decimal('balance', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('cards');
+    }
+}
+
